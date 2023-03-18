76922bfb818d0a195f1a717550499864a4f9d210
victorvadmandjensen <91823836+vict1311@users.noreply.github.com>
2023-03-18 21:14:03 +0100

    newBot
    Now we save the post IDs in a sheet with the post_bot.py and read these with comment_bot.py. Now we should make it so that these are saved with the comments.

--- comment_bot.py
+++ comment_bot.py
@@ -2,6 +2,7 @@ import praw
 from pathlib import Path
 import pathlib
 from openpyxl import Workbook
+from openpyxl import load_workbook
 import xlsxwriter
 # import login.py file
 import login
@@ -12,61 +13,59 @@ from bs4 import BeautifulSoup
 reddit = login.get_reddit()
 
 # Define subreddit to scrape and number of posts
-subreddit = "botsRights"
-post_number = 1000
+#subreddit = "botsRights"
+#post_number = 1000
 
 
 # Create empty list of data and populate it
-data_list = []
-for submission in reddit.subreddit(subreddit).top(time_filter = "all", limit=None):
-    data_list.append(submission)
+#data_list = []
+#for submission in reddit.subreddit(subreddit).top(time_filter = "all", limit=None):
+ #   data_list.append(submission)
 
 # print(data_list)
 
 # Get the path of the xlsx file for data to be saved in
 a = pathlib.Path(__file__)
-p = a.parents[0] / "python_post_data.xlsx"
+p = a.parents[0] / "python_comment_data.xlsx"
 data_file = Path(p)
+
+post_file = a.parents[0] / "python_post_data.xlsx"
+
 
 # Check if Excel file exists, and if not create it
 if data_file.is_file():
     print("Data file exists")
 else:
-    workbook = xlsxwriter.Workbook("python_post_data.xlsx")
+    workbook = xlsxwriter.Workbook("python_comment_data.xlsx")
     print("Data file has been created")
 
-# Define workbook to write to and file and add column names
+# Define workbook to write to and as well as load post title workbook
 wb = Workbook()
-ws1 = wb.create_sheet(title="Post titles")
-ws1.cell(column=1, row=1).value = "Post title"
-#ws1.cell(column=2, row=1).value = "Post ID"
-
-ws2 = wb.create_sheet(title="Post times")
-ws2.cell(column=1, row=1).value = "Post time"
-
-ws3 = wb.create_sheet(title="Post authors")
-ws3.cell(column=1, row=1).value = "Post author"
-
-ws4 = wb.create_sheet(title="Post IDs")
-ws4.cell(column=1, row=1).value = "Post ID"
-
 ws5 = wb.create_sheet(title="Post comments")
 ws5.cell(column=2, row=1).value = "Post comment"
 ws5.cell(column=1, row=1).value = "Post ID"
 
+wb2 = load_workbook("python_post_data.xlsx")
+ws_titles = wb2["Post IDs"]
+
 number_comments = 0
 
+# loop through the specific rows in the relevant sheet 
+for col in ws_titles:
+    for row in col:
+        print(row.value)
+
 # Iterate over items in data_list and add the title to the Excel sheet
-for data_item in range(1, len(data_list)):
+#for data_item in range(1, len(data_list)):
     # Check if the author is deleted
-    if not data_list[data_item].author:
-        continue
-    ws1.cell(column=1, row=data_item+1).value = data_list[data_item].title
-    ws4.cell(column=1, row=data_item+1).value = data_list[data_item].id
+    #if not data_list[data_item].author:
+    #    continue
+    #ws1.cell(column=1, row=data_item+1).value = data_list[data_item].title
+    #ws1.cell(column=2, row=data_item+1).value = data_list[data_item].id
     # convert Unix time of posts to UTC
-    new_time = datetime.datetime.utcfromtimestamp(data_list[data_item].created_utc)
-    ws2.cell(column=1, row=data_item+1).value = new_time
-    ws3.cell(column=1, row=data_item+1).value = data_list[data_item].author.name
+    #new_time = datetime.datetime.utcfromtimestamp(data_list[data_item].created_utc)
+    #ws2.cell(column=1, row=data_item+1).value = new_time
+    #ws3.cell(column=1, row=data_item+1).value = data_list[data_item].author.name
     #ws4.cell(column=1, row=data_item).value = data_list[data_item].selftext
     #ws5.cell(column=1, row=data_item+1).value = data_list[data_item].id
     #for comment in data_list[data_item].comments:
@@ -76,10 +75,10 @@ for data_item in range(1, len(data_list)):
         #    break
 
 #print number of submissions
-print(f"Number of submissions is: {len(data_list)}")
+#print(f"Number of submissions is: {len(data_list)}")
 
 # print number of comments
-print(f"Number of comments is: {number_comments}, but this is not calculated correctly.")
+#print(f"Number of comments is: {number_comments}, but this is not calculated correctly.")
 
 # Save the workbook
 wb.save(filename=data_file)
